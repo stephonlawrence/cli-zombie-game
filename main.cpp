e678fb9a90aeac6b632c3483ed5c21e41c0f24c1
Stephon Lawrence <stephonlawrence@gmail.com>
2021-08-31 16:06:05 -0400

    added choose your level menu

--- main.cpp
+++ main.cpp
@@ -2,22 +2,48 @@
 #include <curses.h>
 #include <stdio.h>
 #include <string>
+#include <filesystem>
+#include <vector>
 
 #include "headers/Level.h"
 #include "headers/ErrorLog.h"
 #include "headers/GameSystem.h"
 //g++ -o main.o main.cpp -lncurses
 using namespace std;
-
+namespace fs = filesystem;
 
 int main(){
+
+  cout << "Choose a level from the list by typing a number then pressing enter: " << endl;
+
+  string path = "./data";
+  unsigned int choiceCounter = 1;
+  vector<string> levels;
+  for (fs::directory_entry const& entry : fs::directory_iterator(path)) {
+    levels.push_back(entry.path());
+  }
+  for (string entry : levels) {
+    cout << "[" << choiceCounter << "] " << entry << endl;
+    choiceCounter++;
+  }
+  unsigned int choice = 0;
+  do {
+    choice = 1;
+    cin >> choice;
+    if (choice < 1 || choice > levels.size())
+      cout << "That is not an option choose a different one.";
+  }
+  while (choice < 1 || choice > levels.size());
+
+  auto chosenLevel = levels.at(choice - 1);
+
   initscr();
 
   noecho();
   //nocbreak();
   timeout(0);
 
-  GameSystem game("./data/level2.txt");
+  GameSystem game(chosenLevel);
   GameState endState = game.playGame();
 
   const unsigned int tipsLength = 3;
